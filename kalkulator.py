c6082a88e11822ce82ed5e7d32c5abd9ffaa74b8
cimolgoreng-cpu <nadziroqurot@gmail.com>
2026-04-09 08:42:48 +0700

    Update kalkulator.py

--- kalkulator.py
+++ kalkulator.py
@@ -1,1 +1,14 @@
-#please add your code here
+a = float(input("Masukkan angka pertama: "))
+b = float(input("Masukkan angka kedua: "))
+op = input("Operator Hitung (+, -, *, /): ")
+
+if op == "+":
+    print("Hasil:", a + b)
+elif op == "-":
+    print("Hasil:", a - b)
+elif op == "*":
+    print("Hasil:", a * b)
+elif op == "/":
+    print("Hasil:", a / b)
+else:
+    print("Operator tidak valid")
